e23497e2e325b98aa6a5d7201e6ed8951ed5a144
Leon Gabriel <g73346920@gmail.com>
2025-02-01 17:32:34 +0300

    modified

--- fylo/pages/index.tsx
+++ fylo/pages/index.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import Hero from "@/public/illustration-intro.png"
 import { Button } from "@/components/common/Button";
 import Back from "@/public/bg-curvy-desktop.svg"
-
+import access from "@/public/icon-access-anywhere.svg"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -19,7 +19,7 @@ export default function Home() {
     <div>
       <section className="relative w-full flex items-center justify-center text-white">
 
-        <div className="relative inset-0 z-0 md:items-center">
+        <div className="relative inset-0 z-0 items-center">
           <Image className="ml-2"
             src={Hero}
             alt="Hero Background"
@@ -41,17 +41,73 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="relative md:grid-cols-2 items-center">
-        <div className="flex items-center">
-          <Image className="ml-[2rem]"
+      <section className="max-w-xl mx-auto p-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-8 items-center">
+        <div className="justify-center">
+          <Image className=""
             src={Back}
             alt="Background"
             objectFit="cover"
-            height={300}
+            height={400}
             quality={100}
           />
 
+          <div className="flex flex-col items-center text-center bg-gray-800 p-6 rounded-2xl shadow-md">
+            <div className="">
+              <Image
+                src={access}
+                width={80}
+                alt="access to my file"
+                quality={100}
+              />
+            </div>
+            <h3 className="text-xl font-semibold mb-2">Access your files, anywhere</h3>
+            <p className="text-gray-300">The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere.</p>
+          </div>
+
+          <div className="flex flex-col items-center text-center bg-gray-800 p-6 rounded-2xl shadow-md mt-3">
+            <div className="">
+              <Image
+                src={access}
+                width={80}
+                alt="access to my file"
+                quality={100}
+              />
+            </div>
+            <h3 className="text-xl font-semibold mb-2">Access your files, anywhere</h3>
+            <p className="text-gray-300">The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere.</p>
+          </div>
+
         </div>
+        <div className="mt-4">
+
+          <div className="flex flex-col items-center text-center bg-gray-800 p-6 rounded-2xl shadow-md mt-3">
+            <div className="">
+              <Image
+                src={access}
+                width={80}
+                alt="access to my file"
+                quality={100}
+              />
+            </div>
+            <h3 className="text-xl font-semibold mb-2">Access your files, anywhere</h3>
+            <p className="text-gray-300">The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere.</p>
+          </div>
+
+
+          <div className="flex flex-col items-center text-center bg-gray-800 p-6 rounded-2xl shadow-md mt-3">
+            <div className="">
+              <Image
+                src={access}
+                width={80}
+                alt="access to my file"
+                quality={100}
+              />
+            </div>
+            <h3 className="text-xl font-semibold mb-2">Access your files, anywhere</h3>
+            <p className="text-gray-300">The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere.</p>
+          </div>
+        </div>
+
       </section>
     </div>
   );
