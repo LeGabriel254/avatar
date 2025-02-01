0b30f73365fb0a3df0a5c9e84685bf189db4555c
Leon Gabriel <g73346920@gmail.com>
2025-02-01 18:19:58 +0300

    modified

--- fylo/tailwind.config.ts
+++ fylo/tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      backgroundImage: {
+        'custom-pattern': "url('/public/bg.svg')",
+      },
     },
   },
   plugins: [],
