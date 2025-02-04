356d6111f44237b86fe4cd7e609812deba40b9f4
Leon Gabriel <g73346920@gmail.com>
2025-02-04 23:46:04 +0300

    modified

--- fylo/components/common/Button.tsx
+++ fylo/components/common/Button.tsx
@@ -4,6 +4,6 @@ import React from 'react'
 
 export const Button:React.FC<ButtonProps> = ({title, action}) => {
   return (
-   <button onClick={action} className="px-10 py-2 border-none bg-[#86DDE4] rounded-full hover:bg-[#aaeaee] hover:text-white transition-colors duration-300">{title}</button>
+   <button onClick={action} className="px-10 py-2 p-1 border-none bg-[#86DDE4] rounded-full hover:bg-[#aaeaee] hover:text-white transition-colors duration-300">{title}</button>
   )
 }
