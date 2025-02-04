b44140e45352267f935a4c2870491f662c91afb8
Leon Gabriel <g73346920@gmail.com>
2025-02-04 16:11:16 +0300

    card component defined how it will looks like

--- fylo/interfaces/index.ts
+++ fylo/interfaces/index.ts
@@ -8,4 +8,11 @@ export interface ButtonProps{
   title:string
   action?: () => void
   className:string
+};
+
+export interface CardProps{
+  name:string;
+  image:string
+  role:string;
+  paragraph:string;
 }
