1a22db3b9cb0ca698342cd8dfae0819f9a6cb2f0
Leon Gabriel <g73346920@gmail.com>
2025-02-28 15:12:58 +0300

    Chored:Modified to hide the Header & Footer on the landing page.

--- github-user-search/components/Layout/Layout.tsx
+++ github-user-search/components/Layout/Layout.tsx
@@ -1,11 +1,17 @@
 import { ReactComponent } from "@/interfaces"
 import Header from "./Header"
+import { useRouter } from "next/router";
+import Footer from "./Footer";
 
 const Layout: React.FC<ReactComponent> = ({ children }) => {
+  const router = useRouter();
+  const hideLayout = router.pathname === "/";
+
   return (
     <>
-      <Header />
+      {!hideLayout && <Header />}
       <main className="min-h-screen">{children}</main>
+      {!hideLayout && <Footer />}
     </>
   )
 }
